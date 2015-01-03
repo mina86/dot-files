360d63ebdfda053199c0fc4b298987d032f8e3d5
Michal Nazarewicz <mina86@mina86.com>
2015-01-03 21:09:31 +0100

    [shell] Use default sound card in cycle-volume.sh

--- bin/libexec/cycle-volume.sh
+++ bin/libexec/cycle-volume.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-vol=$(amixer -c 0 get Master | grep -o '[0-9][0-9]*%' | tr -d %)
+vol=$(amixer get Master | grep -o '[0-9][0-9]*%' | tr -d %)
 if [ $vol -lt 30 ] || [ $vol -ge 60 ]; then
     vol=30
 else
     vol=$(( (($vol / 10 - 2) % 4 + 3) * 10 ))
 fi
-amixer -c 0 set Master $vol% |tail -n1
+amixer set Master $vol% |tail -n1
