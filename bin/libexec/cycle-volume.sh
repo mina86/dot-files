a0431846c8b7c5140f5a5b2b40b759e2a912bb81
Michal Nazarewicz <mina86@mina86.com>
2015-05-20 16:01:13 +0200

    [sawfish] Replace cycle-volume.sh with explicit volume setting
    Having to cycle through various volume levels is somehow cumbersome when
    wanting to go back a level.  What’s more, to get to lower volume one must
    first go through higher volume levels.  Replace the cycle-volume.sh script
    with two-keystroke bindings for setting volume to desired levels.

--- bin/libexec/cycle-volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-vol=$(amixer get Master | grep -o '[0-9][0-9]*%' | tr -d %)
-if [ $vol -lt 30 ] || [ $vol -ge 60 ]; then
-    vol=30
-else
-    vol=$(( (($vol / 10 - 2) % 4 + 3) * 10 ))
-fi
-amixer set Master $vol% |tail -n1
