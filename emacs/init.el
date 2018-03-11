1c30de35f413f566fb7c791be4a7c2c2ede8bd44
Michal Nazarewicz <mina86@mina86.com>
2018-03-11 12:53:52 +0000

    [emacs] Remove unnecessary indentation

--- emacs/init.el
+++ emacs/init.el
@@ -5,10 +5,10 @@
 
 ;;; Code:
 
-  ;; Mitigate Bug#28350 (security) in Emacs 25.2 and earlier.
-  (eval-after-load "enriched"
-    '(defun enriched-decode-display-prop (start end &optional param)
-       (list start end)))
+;; Mitigate Bug#28350 (security) in Emacs 25.2 and earlier.
+(eval-after-load "enriched"
+  '(defun enriched-decode-display-prop (start end &optional param)
+     (list start end)))
 
 ;;{{{ System dependend data and directories
 
