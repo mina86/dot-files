7521788843c1c509aff7927bf8184677acaf9ab6
Michal Nazarewicz <mina86@mina86.com>
2024-06-02 04:26:57 +0200

    [bin] Fix -L handling in pcd.sh

--- bin/pcd.sh
+++ bin/pcd.sh
@@ -1,50 +1,52 @@
 # shellcheck shell=sh disable=SC2164
 
 cd() {
-	case "$#:$1" in
-	1:-h)
-		cat <<EOF
-cd                                     Move to home directory.
-cd -P                                  Resolve all symbolic links in \$PWD.
-cd [-L | -P | --] <target>             Move to given <target>.
-cd [-L | -P | --] <target> <cmd> ...   Run <cmd> in the given <target>.
-
-<target> can be:
-  -           Move to directory stored in \$OLDPWD
-  .../<path>  Move to a sibling/cusing directory <path>; this will iterate
-              up the directories until <path> is located
-  <file>      Move to directory containing the file
-  <dir>       Move to given directory
-EOF
-		;;
+	case $#:$1 in
 	0:)
 		command cd
+		return
 		;;
+
+	1:--help)
+		cat <<EOF
+Usage:
+   cd                                    Move to home directory.
+   cd -P                                 Resolve all symbolic links in \$PWD.
+   cd [-L | -P | --] <target>            Move to given <target>.
+   cd [-L | -P | --] <target> <cmd>...   Run <cmd> in the given <target>.
+
+<target> can be:
+   -            Move to directory stored in \$OLDPWD.
+   .../<path>   Move to a sibling/cusing directory <path>; this will iterate
+                  up the directories until <path> is located.
+   <file>       Move to directory containing the file.
+   <dir>        Move to given directory.
+EOF
+		return
+		;;
+
 	1:-[LP])
-		command cd "$1" "${PWD}"
+		set -- "$1" "${PWD}"
 		;;
 	1:*)
-		_cd_do -L "$1"
-		;;
-	2:--)
-		_cd_do -L "$2"
-		;;
-	2:-[LP])
-		_cd_do "$1" "$2"
-		;;
-	*)
-		if [ "$1" = -- ]; then
-			shift
-			set -- -L "$@"
-		elif [ "$1" != -P ]; then
-			set -- -L "$@"
-		fi
-		( _cd_do "$1" "$2" && shift 2 && exec "$@" )
+		set -- -L "$1"
 	esac
+
+	case $1 in
+	--)     shift; set -- -L "$@"; ;;
+	-[LP])  :; ;;
+	*)      set -- -L "$@"
+	esac
+
+	if [ $# -eq 2 ]; then
+		_cd_do "$@"
+	else
+		( _cd_do "$1" "$2" && shift 2 && exec "$@" )
+	fi
 }
 
 _cd_do() {
-	case "$2" in
+	case $2 in
 	-)
 		:
 		;;
